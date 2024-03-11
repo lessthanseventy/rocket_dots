bfc95437ac97751638c12d1d1b491c62656cd46f
Andrew Moore <andrew@lessthanseventy.com>
2024-03-11 17:10:19 -0600

    cleanup

--- waybar/.config/waybar/scripts/headsetctl.sh
+++ waybar/.config/waybar/scripts/headsetctl.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 battery_level=$(headsetcontrol -b | grep "Battery" | awk '{print $2}' | tr -d '%')
 if [[ $battery_level =~ "Charging" ]]; then
-	printf '{"text": "  %s", "tooltip": "%s"}\n' "$battery_level"
+	printf '{"text": "󰋎  %s", "tooltip": "%s"}\n' "$battery_level"
+if [[ $battery_level =~ "Unavailable" ]]; then
+	printf '{"text": "", "tooltip": ""}\n'
 elif [[ -n $battery_level ]]; then
-	printf '{"text": "  %s%%", "tooltip": "%s"}\n' "$battery_level"
+	printf '{"text": "󰋏  %s%%", "tooltip": "%s"}\n' "$battery_level"
 else
 	printf '{"text": "", "tooltip": ""}\n'
 fi
